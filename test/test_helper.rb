2147b704d7ab99990bdb22c24a9f59dc3c089380
Orion Osborn <orion.osborn@gmail.com>
2015-01-17 15:43:18 -0700

    add ability to logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,15 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/mini_test'
 require 'capybara/rails'
+require 'minitest/pride'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def teardown
+    User.destroy_all
+  end
 end
